a97f79887dcc351c44bcd6b28e45af5b856fb9c5
EduTechLab.ita <edutechlab.ita@gmail.com>
2025-08-03 19:55:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // IMPORTANTE: Incrementa questa versione a 'v3', 'v4', ecc. ad ogni nuovo aggiornamento!
-const CACHE_NAME = 'edutech-lab-v4'; 
+const CACHE_NAME = 'edutech-lab-v5'; 
 const urlsToCache = [
   './',
   './index.html',
